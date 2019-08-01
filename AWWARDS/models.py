f35b8172c9704c56ad6d506112d320dca91e71a2
peter max <petermax2004@gmail.com>
2019-08-01 12:48:32 +0300

    Add the str,save and delete projects functions to the projects class

--- AWWARDS/models.py
+++ AWWARDS/models.py
@@ -25,3 +25,13 @@ class Projects(models.Model):
     link = models.CharField(max_length=100)
     profile = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
     pub_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+    def save_project(self):
+        self.save()
+        
+    def delete_project(self):
+        self.delete
+        
