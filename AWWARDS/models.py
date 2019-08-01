505a43cbd915a3d64101060684a51682e04087a6
peter max <petermax2004@gmail.com>
2019-08-01 12:40:57 +0300

    Change position of the profile class

--- AWWARDS/models.py
+++ AWWARDS/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    profile_picture=models.ImageField(upload_to = 'images/')
+    bio=models.CharField(max_length=100)
+    contact=models.CharField(max_length=50)
+    user=models.OneToOneField(User,null=True)
+    pub_date=models.DateTimeField(auto_now_add=True)
+    
 class Projects(models.Model):
     title = models.CharField(max_length=30)
     image = models.ImageField(upload_to = 'images')
@@ -8,10 +16,3 @@ class Projects(models.Model):
     link = models.CharField(max_length=100)
     profile = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
     pub_date = models.DateTimeField(auto_now_add=True)
-    
-class Profile(models.Model):
-    profile_picture=models.ImageField(upload_to = 'images/')
-    bio=models.CharField(max_length=100)
-    contact=models.CharField(max_length=50)
-    user=models.OneToOneField(User,null=True)
-    pub_date=models.DateTimeField(auto_now_add=True)
