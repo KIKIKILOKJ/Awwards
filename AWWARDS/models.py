c81ee33ddecd7ce4396ecbd5af3a9901cd0eceee
peter max <petermax2004@gmail.com>
2019-08-01 12:55:50 +0300

    Add the rate review class and the save function

--- AWWARDS/models.py
+++ AWWARDS/models.py
@@ -8,16 +8,16 @@ class Profile(models.Model):
     contact=models.CharField(max_length=50)
     user=models.OneToOneField(User,null=True)
     pub_date=models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.user
-    
+
     def save_profile(self):
         self.save()
-        
+
     def delete_profile(self):
         self.delete()
-    
+
 class Projects(models.Model):
     title = models.CharField(max_length=30)
     image = models.ImageField(upload_to = 'images')
@@ -25,13 +25,20 @@ class Projects(models.Model):
     link = models.CharField(max_length=100)
     profile = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
     pub_date = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.title
-    
+
     def save_project(self):
         self.save()
-        
+
     def delete_project(self):
         self.delete
-        
+
+class RateReview(models.Model):#
+    design=models.PositiveIntegerField(validators=[MinValueValidator(0),MaxValueValidator(10)])
+    usability = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
+    content = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
+
+    def save_ratereview(self):
+        self.save()
