ad9c54b7df5e3147423af2079e250fbf3ad2025e
peter max <petermax2004@gmail.com>
2019-08-01 12:10:49 +0300

    Add the Projects class with the title,image,description,link,profile and pubdate fields

--- AWWARDS/models.py
+++ AWWARDS/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Projects(models.Model):
+    title = models.CharField(max_length=30)
+    image = models.ImageField(upload_to = 'ards/',default='Project Image')
+    description = models.CharField(max_length=300)
+    link = models.CharField(max_length=100)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
+    pub_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
