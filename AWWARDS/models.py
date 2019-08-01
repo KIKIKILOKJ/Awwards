fc70e86f135029e1bdac540b90fbb88b991d518e
peter max <petermax2004@gmail.com>
2019-08-01 12:39:42 +0300

    Add the class profile with the profile,bio,contact,user and pubdate fields

--- AWWARDS/models.py
+++ AWWARDS/models.py
@@ -3,8 +3,15 @@ from django.db import models
 # Create your models here.
 class Projects(models.Model):
     title = models.CharField(max_length=30)
-    image = models.ImageField(upload_to = 'ards/',default='Project Image')
+    image = models.ImageField(upload_to = 'images')
     description = models.CharField(max_length=300)
     link = models.CharField(max_length=100)
     profile = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
-    pub_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+class Profile(models.Model):
+    profile_picture=models.ImageField(upload_to = 'images/')
+    bio=models.CharField(max_length=100)
+    contact=models.CharField(max_length=50)
+    user=models.OneToOneField(User,null=True)
+    pub_date=models.DateTimeField(auto_now_add=True)
