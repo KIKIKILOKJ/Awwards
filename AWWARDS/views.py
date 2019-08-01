88b6ff2d40a73ecf5cbc34a1ca81af2468da864a
peter max <petermax2004@gmail.com>
2019-08-01 14:06:35 +0300

    Add the convert date function to convert the dates for monday-sunday

--- AWWARDS/views.py
+++ AWWARDS/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import datetime as dt
 
 # Create your views here.
+def convert_dates(dates):
+    # function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','thursday','Friday','Saturday','Sunday']
+    '''
+    Returns the actual day of the week
+    '''
+    day = days[day_number]
+    return day
